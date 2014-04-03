bf85188c311a830909f99d3378b8ab5b7297e77a
Akira Iketani <mr870051@mr.furukawa.co.jp>
2014-04-03 11:28:45 +0900

    2014/4/3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,80 +2,79 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  #let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      expect(page).not_to have_title('| Home')
     end
+
   end
 
 
   describe "Help page" do
 
-    it "should have the h1 'Help'" do
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      expect(page).to have_content('Help')
     end
 
     it "should have the base title" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit '/static_pages/help'
-      page.should_not have_selector('title', :text => '| Help')
+      expect(page).not_to have_title('| Help')
     end
   end
 
   describe "About page" do
 
-    it "should have the h1 'About Us'" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      expect(page).to have_content('About Us')
     end
 
     it "should have the base title" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit '/static_pages/about'
-      page.should_not have_selector('title', :text => '| about')
+      expect(page).not_to have_title('| About')
     end
   end
 
   describe "Contact page" do
 
-    it "should have the h1 'Contact'" do
+    it "should have the content 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'contact')
+      expect(page).to have_content('Contact')
     end
 
     it "should have the base title" do
       visit '/static_pages/contact'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit '/static_pages/contact'
-      page.should_not have_selector('title', :text => '| Contact')
+      expect(page).not_to have_title('| Contact')
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@ Spork.prefork do
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  
+  # Checks for pending migrations before tests are run.
+  # If you are not using ActiveRecord, you can remove this line.
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
     # == Mock Framework
@@ -23,7 +27,7 @@ Spork.prefork do
     # config.mock_with :mocha
     # config.mock_with :flexmock
     # config.mock_with :rr
-    config.mock_with :rspec
+  #  config.mock_with :rspec
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -37,6 +41,14 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+    
+    # Run specs in random order to surface order dependencies. If you 
+    # find an order dependency and want to debug it, you can fix the
+    # order by providing the seed, which is printed after each run.
+    #     --seed 1234
+	config.order = "random"
+    config.include Capybara::DSL #2014/4/1
+    #config.include Rails.application.routes.url_helpers #2014/4/1
   end
 end
 
